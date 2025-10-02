050113fcf0ff547a6c0f2c77582544370091d2da
Luke Sandberg <lukeisandberg@gmail.com>
2025-10-01 17:04:54 -0700

    set turbopack_build env variables in test-new-tests
    these really shouldn't be used, btu they are

--- scripts/test-new-tests.mjs
+++ scripts/test-new-tests.mjs
@@ -168,7 +168,8 @@ async function main() {
           NEXT_TEST_VERSION: nextTestVersion,
           NEXT_EXTERNAL_TESTS_FILTERS,
           IS_TURBOPACK_TEST: '1',
-          TURBOPACK_BUILD: testMode === 'start' ? '1' : undefined,
+          TURBOPACK_BUILD:
+            testMode === 'start' || testMode === 'deploy' ? '1' : undefined,
           TURBOPACK_DEV: testMode === 'dev' ? '1' : undefined,
         },
       })
